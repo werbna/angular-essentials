5c1e84ada2e40abc31cfbc61201cf0053e9ac719
Andrew <andrewchau1993@gmail.com>
2024-12-03 15:39:11 -0800

    introduced signals, you may assign a default value to be assumed if nothing is passed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 @Component({
@@ -9,8 +9,8 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string; 
-  @Input({required: true}) name!: string; 
+  avatar = input('');
+  name = input('');
 
   get imagePath() {
     return `assets/users/` + this.avatar;
