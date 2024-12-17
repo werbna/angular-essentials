597f4ae2daf9602fd75e257dd3a393f5df2d55d9
Andrew <andrewchau1993@gmail.com>
2024-12-16 22:13:18 -0800

    removed signals because most companies havent quite updated to future versions of angular

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,19 +9,13 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string; 
-  // @Input({required: true}) name!: string; 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string; 
+  @Input({required: true}) name!: string; 
 
-
-  imagePath = computed(() => {
-    return `assets/users/` + this.avatar();
-  });
   
-  // get imagePath() {
-  //   return `assets/users/` + this.avatar;
-  // }
+  get imagePath() {
+    return `assets/users/` + this.avatar;
+  }
 
   onSelectUser() {}
 }
