1f1ee2497bdf19810e83db84605266781a16afe6
Andrew <andrewchau1993@gmail.com>
2024-12-17 15:59:12 -0800

    note that when exporting information, one must specify what its going to be pulling from the data, also make sure what im pulling is similar in what im calling it as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,13 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'U1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!
+  }
+
   onSelectUser(id:string) {
-    console.log('User selected: ', id);
+    this.selectedUserId = id;
   }
 }
