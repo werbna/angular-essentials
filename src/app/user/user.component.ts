4fe8ecc5949bf05b6dc64d36b6ccc200ea735df2
Andrew <andrewchau1993@gmail.com>
2024-12-16 22:22:06 -0800

    the selector is going to choose based off of id, so now im bringing in id

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, Output, EventEmitter  } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,13 +7,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string; 
-  @Input({required: true}) name!: string; 
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
 
-  
   get imagePath() {
     return `assets/users/` + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
